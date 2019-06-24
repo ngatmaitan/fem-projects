47d3c0129ff0b4981ad2da9edc3df58aa9dbb708
Nicole Gatmaitan <ngatmaitan@gmail.com>
2019-06-24 14:34:52 -0500

    completed destructuring exercise

---
+++ Beginner Learning Path/Javascript From Fundamentals to Functional JS v2/Destructuring/exercise.js
@@ -0,0 +1,6 @@
+// Create an object that looks like this:
+// {"name": Rusty, "room":"kitchen", "weapon": "candlestick"}
+const {name, room, weapon} = {name: "Rusty", room: "kitchen", weapon: "candlestick"}
+
+// Extract out the weapon and location using destructuring
+weapon
